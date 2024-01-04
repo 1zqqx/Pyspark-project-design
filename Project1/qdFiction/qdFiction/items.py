ecf1b51f8f5ada45ba6ffab53d75e74828ebeed3
this1slife <3522954997@qq.com>
2024-01-04 21:33:43 +0800

    qd spider for all creation

---
+++ Project1/qdFiction/qdFiction/items.py
@@ -0,0 +1,21 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class QdfictionItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # book_link = scrapy.Field()  # 书连接
+    # book_cover = scrapy.Field()  # 封面连接
+    book_name = scrapy.Field()  # 书名
+    book_author = scrapy.Field()  # 作者
+    book_first_partition = scrapy.Field()  # 一级分区
+    book_second_partition = scrapy.Field()  # 二级分区
+    book_status = scrapy.Field()  # 连载/完结
+    book_intro = scrapy.Field()  # 书介绍
+    book_wordage = scrapy.Field()  # 书字数
+    pass
---
+++ Project1/qdFiction/qdFiction/spiders/qd.py
@@ -0,0 +1,33 @@
+import scrapy
+
+from ..items import QdfictionItem
+
+# 全部作品 100 条
+
+class QdSpider(scrapy.Spider):
+    name = "qd"
+    allowed_domains = ["www.qidian.com"]
+    start_urls = ["https://www.qidian.com/all/page5/"]
+
+    def parse(self, response):
+        li_list = response.xpath('//div[@class="book-mid-info"]')
+
+        for item in li_list:
+            book = QdfictionItem()
+            book['book_name'] = item.xpath('h2/a/text()').get().strip()
+            book['book_author'] = item.xpath('p/a[@class="name"]/text()').get().strip()
+            book['book_first_partition'] = item.xpath('p/a[2]/text()').get().strip()
+            book['book_second_partition'] = item.xpath('p/a[3]/text()').get().strip()
+            book['book_status'] = item.xpath('p/span/text()').get().strip()
+            book['book_intro'] = item.xpath('p[@class="intro"]/text()').get().strip()
+            book['book_wordage'] = item.xpath('p[@class="update"]/span/span/text()').get().strip() + "万字"
+            yield book
+
+        # 起点网站第四页没有去往第五页的链接 但是可以倒着来 从第五页向前
+        next_url = response.xpath(
+            '//div[@class="lbf-pagination"]/ul/li[1]/a/@href').get()
+        if next_url != 'javascript:;' and next_url is not None:
+            next_url = 'https:' + next_url
+            print("[=] " + next_url)
+            yield scrapy.Request(url=next_url, callback=self.parse)
+        pass
